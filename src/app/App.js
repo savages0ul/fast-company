cf3b5bec27022019058343d1490a1780f9e8c0e4
Roman Kashin <kashin071@mail.ru>
2022-02-20 14:36:24 +0300

    added protectedRoute

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,7 @@ import NavBar from './components/ui/navBar';
 import { ProfessionProvider } from './hooks/useProfession';
 import { QualitiesProvider } from './hooks/useQualities';
 import AuthProvider from './hooks/useAuth';
+import ProtectedRoute from './components/common/protectedRoute';
 
 function App() {
     return (
@@ -17,7 +18,7 @@ function App() {
                 <QualitiesProvider>
                     <ProfessionProvider>
                         <Switch>
-                            <Route
+                            <ProtectedRoute
                                 path="/users/:userId?/:edit?"
                                 component={Users}
                             />
