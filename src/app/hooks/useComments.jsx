54c813d0ba430cd1b5842a8c4f66d46c718a263e
Roman Kashin <kashin071@mail.ru>
2022-02-20 19:10:34 +0300

    Добавлен запрос добавления комментария

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import { useParams } from 'react-router-dom';
 import { useAuth } from './useAuth';
 import { nanoid } from 'nanoid';
+import commentService from '../services/comment.service';
 
 const CommentsContext = React.createContext();
 
@@ -13,12 +15,13 @@ export const useComments = () => {
 export const CommentsProvider = ({ children }) => {
     const { userId } = useParams();
     const { currentUser } = useAuth();
-    // const [isLoading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(true);
     const [comments, setComments] = useState([]);
-    // const [error, setError] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setComments(null);
+        setLoading(false);
     }, []);
 
     async function createComment(data) {
@@ -30,11 +33,31 @@ export const CommentsProvider = ({ children }) => {
             created_at: Date.now(),
             userId: currentUser._id
         };
+        try {
+            const { content } = await commentService.createComment(comment);
+            console.log(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
         console.log(comment);
     }
 
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
     return (
-        <CommentsContext.Provider value={{ comments, createComment }}>
+        <CommentsContext.Provider
+            value={{ comments, createComment, isLoading }}
+        >
             {children}
         </CommentsContext.Provider>
     );
